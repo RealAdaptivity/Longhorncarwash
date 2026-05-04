daa2a0f677a3e0e31508c4d4d80a2d642cc68cd6
TrixyLab <75338893+TrixyLab@users.noreply.github.com>
2026-05-04 13:27:42 -0500

    Add files via upload

---
+++ src/supabaseConfig.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://pbgatghmutejbsmcedsw.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_WwJQsA6iuQcMiCP6vZWMgw_71wB5smo';
+
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
